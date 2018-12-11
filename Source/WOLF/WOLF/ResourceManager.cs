a48a5a347ee18a1656888983989994a05aee25a3
Bob Palmer <rbpalmer_nrt@hotmail.com>
2018-12-11 12:25:32 -0500

    Use SQRT of body radius as part of the abundance equation

--- Source/WOLF/WOLF/ResourceManager.cs
+++ Source/WOLF/WOLF/ResourceManager.cs
@@ -10,15 +10,17 @@ namespace WOLF
         public const int RESOURCE_ABUNDANCE_CEILING = 1000;
         public const int RESOURCE_ABUNDANCE_FLOOR = 1;
         public const int RESOURCE_ABUNCANCE_MULTIPLIER = 100;
+        public const double RESOURCE_ABUNDANCE_RADIUS_MULT = 250d;
 
         public static Dictionary<string, int> GetResourceAbundance(List<string> resources, AbundanceRequest abReq)
         {
             var resourceList = new Dictionary<string, int>();
+            var radiusMult = Math.Sqrt(FlightGlobals.Bodies[abReq.BodyId].Radius) / RESOURCE_ABUNDANCE_RADIUS_MULT;
             foreach(var resource in resources)
             {
                 abReq.ResourceName = resource;
                 var baseAbundance = ResourceMap.Instance.GetAbundance(abReq);
-                int abundance = (int)(baseAbundance * (double)RESOURCE_ABUNCANCE_MULTIPLIER);
+                int abundance = (int)(baseAbundance * (double)RESOURCE_ABUNCANCE_MULTIPLIER * radiusMult);
                 if(abundance > RESOURCE_ABUNDANCE_FLOOR)
                 {
                     abundance = Math.Min(abundance, RESOURCE_ABUNDANCE_CEILING);
